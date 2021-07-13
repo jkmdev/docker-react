a7ecf915db08e1685b56d76bc6b0e7fe3a71e795
Julia McGeoghan <jkmcgeoghan@myseneca.ca>
2021-07-13 15:04:41 -0400

    Add class based component as an exercise

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+    return <div> S D</div>;
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      lat: 0, errorMessage: 'woof'
+    };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    // return (
+    //   <div>
+    //     Latitude: {this.state.lat} 
+    //     <br />
+    //     Error: {this.state.errorMessage}
+    //   </div>
+    // );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat} </div>
+    }
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,
+  document.querySelector('#root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
